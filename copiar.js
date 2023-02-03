34b86a339fd42e5f7d31ac6fa97dea50825947b7
frankabrera <119618921+frankabrera@users.noreply.github.com>
2023-02-03 12:14:28 -0600

    Delete copiar.js

--- copiar.js
+++
@@ -1,6 +0,0 @@
-function copiar() {
-    navigator.clipboard.writeText(
-        document.getElementById("encrypted-text").value
-    );
-}
-document.querySelector("#boton-copiar").addEventListener("click", copiar);
